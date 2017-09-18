5c0bd3f953b3abb122982ee0b644576b8e2c1065
AndrHend062 <andrhend062@ed.amdsb.ca>
2017-09-18 13:03:58 -0400

    Add project files.

---
+++ Play game/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Play_game
+{
+    partial class playGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(228, 47);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(211, 132);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.counterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.Location = new System.Drawing.Point(197, 245);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(275, 38);
+            this.counterLabel.TabIndex = 1;
+            this.counterLabel.Text = "Starting In 3";
+            this.counterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.counterLabel.Visible = false;
+            // 
+            // playGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(684, 381);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.playButton);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "playGame";
+            this.Text = "Play Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label counterLabel;
+    }
+}
+
---
+++ Play game/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Play_game
+{
+    public partial class playGame : Form
+    {
+        public playGame()
+        {
+            InitializeComponent();
+        }
+        SoundPlayer player = new SoundPlayer(Properties.Resources.Alarm);
+        SoundPlayer player1 = new SoundPlayer(Properties.Resources.up);  
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            playButton.Visible = false;
+            counterLabel.Visible = true;
+
+            player1.Play(); 
+
+            counterLabel.Text = "Starting In 3";
+            this.Refresh(); //refresh 
+            Thread.Sleep(1000);
+
+            player1.Play();
+            counterLabel.Text = "Starting In 2";
+            this.Refresh(); //refresh 
+            Thread.Sleep(1000);
+
+            player1.Play(); 
+            counterLabel.Text = "Starting In 1";
+            this.Refresh(); //refresh
+            Thread.Sleep(1000);
+           
+            counterLabel.Text = "GO"; // display go 
+            BackColor = Color.Green; //change background colour
+            player.Play(); //play sound 
+            Refresh();
+            Thread.Sleep(2000);
+            player.Stop();
+
+            counterLabel.Visible = false;
+            this.Refresh();
+            //logo
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Red, 10);
+            SolidBrush drawBrushred = new SolidBrush(Color.Red);
+            SolidBrush drawBrushBlack = new SolidBrush(Color.Black);
+            formGraphics.Clear(Color.Green);
+
+            Font drawFont = new Font("Arial", 16, FontStyle.Bold); // creat new font 
+            Font drawFontBig = new Font("DejaVu Serif", 66, FontStyle.Bold);//create anouther font 
+
+
+            formGraphics.FillEllipse(drawBrushred, 225, 100, 200, 200); // draw full ellipse
+
+            formGraphics.DrawString("Rams", drawFont, drawBrushBlack, 265, 200);  // display rams
+            formGraphics.DrawString("C", drawFontBig, drawBrushBlack, 230, 160);  // display C
+
+            formGraphics.TranslateTransform(380, 330); // upsidedown text 
+            formGraphics.RotateTransform(180);
+            formGraphics.DrawString("Welcome", drawFont, drawBrushBlack, new Rectangle());
+            formGraphics.ResetTransform();
+
+        }
+    }
+}
